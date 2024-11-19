1d5ba88145e12dfcee6cc0b2c8bd43226fba6c61
ashenbgy <ashenbhagye@gmail.com>
2024-11-19 12:09:01 +0530

    Initial Commit.

---
+++ src/main/java/com/alpha/redis/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.alpha.redis.controller;
+
+import com.alpha.redis.entity.Product;
+import com.alpha.redis.respository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProduct(@PathVariable int id) {
+        return productRepository.findProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable int id)   {
+        return productRepository.deleteProduct(id);
+    }
+}
