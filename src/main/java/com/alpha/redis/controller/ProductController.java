41e1057cc29b2c284672c5362b3b5ed2b41638fe
ashenbgy <ashenbhagye@gmail.com>
2024-11-19 12:55:23 +0530

    Updated Spring Data Redis as Cache

--- src/main/java/com/alpha/redis/controller/ProductController.java
+++ src/main/java/com/alpha/redis/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.alpha.redis.controller;
 import com.alpha.redis.entity.Product;
 import com.alpha.redis.respository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,11 +26,13 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
+    @Cacheable(key = "#id",value = "Product",unless = "#result.price > 1000")
     public Product findProduct(@PathVariable int id) {
         return productRepository.findProductById(id);
     }
 
     @DeleteMapping("/{id}")
+    @CacheEvict(key = "#id",value = "Product")
     public String remove(@PathVariable int id)   {
         return productRepository.deleteProduct(id);
     }
